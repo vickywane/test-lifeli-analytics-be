644c234dc2dabcc6a33001cc262e4b8212724689
rewdt <andrewbamidele2@gmail.com>
2020-04-22 04:09:02 +0100

    optimize backend code for speed

--- src/routes/api/web/index.js
+++ src/routes/api/web/index.js
@@ -11,43 +11,76 @@ const router = express.Router();
 // }
 
 router.get("/fetch-all-events", async (req, res) => {
-  await userEvents.find({}, (err, data) => {
-    if (err) {
-      return res.status(400).send({
-        status: "error",
-        message: "An error occurred while attempting to pull events"
-      });
-    }
-    return res.send({ status: "success", data });
-  });
+  await userEvents
+    .find({})
+    .populate({
+      path: "_user",
+      select: "uuid notification_settings.user_timezone"
+    })
+    .exec((err, data) => {
+      if (err) {
+        return res.status(400).send({
+          status: "error",
+          message: "An error occurred while attempting to pull events"
+        });
+      }
+      return res.send({ status: "success", data });
+    });
 });
 
-router.get("/fetch-user-activities", async (req, res) => {
-  const [getAllUsers, getUserEvents] = await Promise.all([
-    user.find({}),
-    userEvents.find({})
-  ]);
-  const allusers = [];
-  getAllUsers.forEach(async (user) => {
-    const eventsPerUser = getUserEvents.filter(
-      (event) => event.uuid === user.uuid
-    );
-    const hoursTracked = eventsPerUser.reduce((acc, currentValue) => {
-      const start_time = moment(currentValue.time_schedule.start_time);
-      const end_time = moment(currentValue.time_schedule.end_time);
+// router.get("/fetch-user-activities", async (req, res) => {
+//   await userEvents
+//     .find({})
+//     .populate({
+//       path: "_user",
+//       select: "uuid notification_settings.user_timezone"
+//     })
+//     .exec((err, data) => {
+//       if (err) {
+//         return res.status(400).json(err);
+//       }
+//       return res.send({ status: "success", data });
+//     });
+//   // const [getAllUsers, getUserEvents] = await Promise.all([
+//   //   user.find({}),
+//   //   userEvents.find({})
+//   // ]);
+//   // const allusers = [];
+//   // getAllUsers.forEach(async (user) => {
+//   //   const eventsPerUser = getUserEvents.filter(
+//   //     (event) => event.uuid === user.uuid
+//   //   );
+//   //   const hoursTracked = eventsPerUser.reduce((acc, currentValue) => {
+//   //     const start_time = moment(currentValue.time_schedule.start_time);
+//   //     const end_time = moment(currentValue.time_schedule.end_time);
 
-      return moment.duration(end_time.diff(start_time)).asHours() + acc;
-    }, 0);
+//   //     return moment.duration(end_time.diff(start_time)).asHours() + acc;
+//   //   }, 0);
 
-    allusers.push({
-      uuid: user.uuid,
-      timezone: user.notification_settings.user_timezone,
-      no_of_events: eventsPerUser.length || 0,
-      hoursTracked
+//   //   allusers.push({
+//   //     uuid: user.uuid,
+//   //     timezone: user.notification_settings.user_timezone,
+//   //     no_of_events: eventsPerUser.length || 0,
+//   //     hoursTracked
+//   //   });
+//   // });
+
+//   // return res.send({ status: "success", data: allusers });
+// });
+
+router.get("/fetch-all-users", (req, res) => {
+  user
+    .find({})
+    .select("uuid notification_settings.user_timezone")
+    .exec((err, data) => {
+      if (err) {
+        return res.status(400).send({
+          status: "error",
+          message: "Unable to complete fetching of events"
+        });
+      }
+      return res.send({ status: "success", data });
     });
-  });
-
-  return res.send({ status: "success", data: allusers });
 });
 
 export default router;
